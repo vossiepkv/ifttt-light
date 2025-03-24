203059f73450b7ca4f79219432b6c6d6562df28d
vossiepkv <vossiepkv@gmail.com>
2025-03-24 21:43:23 +1100

    updated code

--- ifttt-light.ino
+++ ifttt-light.ino
@@ -2,11 +2,8 @@
 #include <WiFiNINA.h>
 #include <Wire.h>
 #include <BH1750.h>
-  
+
 BH1750 GY30; // instantiate a sensor event object
-
-
-
 
 #include "Secret.h" 
 // Please enter your sensitive data in the Secret tab/arduino_secrets.h
@@ -15,12 +12,11 @@ char pass[] = SECRET_PASS;    // Your network password (use for WPA, or use as k
 int status = WL_IDLE_STATUS;     // The WiFi radio's status
 WiFiClient  client;
 
-//defines IFTTT event webhook listeners
+// Defines IFTTT event webhook listeners
 char   HOST_NAME[] = "maker.ifttt.com";
-String PATH_NAME   = "/trigger/Sunlight-detected-over-terranium/with/key/bWL5vFJUyBZMaaYxHoKZx6"; // change your EVENT-NAME and YOUR-KEY
-String queryString = "?value1=57&value2=25";
 char   HOST_NAME1[] = "maker.ifttt.com";
-String PATH_NAME1   = "/trigger/sun-off-terrarium/with/key/bWL5vFJUyBZMaaYxHoKZx6"; // change your EVENT-NAME and YOUR-KEY
+String PATH_NAME   = "/trigger/sun-on/with/key/iAGT0WbFP-AP71q4ClRiDVukOYDMnBgMoOzvgGsaBN-";
+String PATH_NAME1  = "/trigger/sun-off/with/key/iAGT0WbFP-AP71q4ClRiDVukOYDMnBgMoOzvgGsaBN-";
 
 
 void setup() {
@@ -30,88 +26,77 @@ void setup() {
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
     status = WiFi.begin(ssid, pass);
-    // Wait 10 seconds for connection:
-    delay(10000);
+    delay(10000); // Wait 10 seconds for connection
   }
   Serial.println("Connected to WiFi");
 
-    Wire.begin(); // Initialize the I2C bus for use by the BH1750 library  
+  Wire.begin(); // Initialize the I2C bus  
   GY30.begin(); // Initialize the sensor object
 
-if (client.connect(HOST_NAME, 80)) {
-    // if connected:
+  if (client.connect(HOST_NAME, 80)) {
     Serial.println("Connected to server");
+  } else {
+    Serial.println("Connection failed");
   }
-  else {// if not connected:
-    Serial.println("connection failed");
-  }
-
 }
-
 
 bool aboveThresholdNotified = false; // Flag to track if notification for lux > 300 has been sent
 bool belowThresholdNotified = false; // Flag to track if notification for lux < 300 has been sent
 
 void loop() {
-  float lux = GY30.readLightLevel(); // read the light level from the sensor and store it in a variable
-  Serial.println(lux); // print the data to the serial monitor
+  float lux = GY30.readLightLevel(); // Read the light level from the sensor
+  Serial.println(lux); // Print the data to the serial monitor
   delay(1000); // Pause for a second before repeating the sensor poll
 
-  if (lux >= 300.00) { // Send trigger if lux exceeds 300.00
-    if (!aboveThresholdNotified) { // Check if notification has not been sent yet
-      aboveThresholdNotified = true; // Set the flag to indicate notification sent
-      belowThresholdNotified = false; // Reset the flag for below threshold notification
+  // Create query string here after lux is declared
+  String queryString = "?value1=" + String(lux) + "&value2=" + String(lux);
 
-      // Disconnect from the server to allow new trigger
+  if (lux >= 100.00) { 
+    if (!aboveThresholdNotified) { 
+      aboveThresholdNotified = true; 
+      belowThresholdNotified = false;
+
       if (client.connected()) {
         client.stop();
         Serial.println("Disconnected from server");
       }
 
-      // Attempt to reconnect to the server
       if (client.connect(HOST_NAME, 80)) {
         Serial.println("Connected to server");
       } else {
         Serial.println("Connection failed");
       }
 
-      // make a HTTP request for lux > 300 notification:
-      // send HTTP header
+      // Send HTTP request
       client.println("GET " + PATH_NAME + queryString + " HTTP/1.1");
       client.println("Host: " + String(HOST_NAME));
       client.println("Connection: close");
-      client.println(); // end HTTP header
+      client.println();
     }
-  } else { // lux < 300
-    if (!belowThresholdNotified) { // Check if notification has not been sent yet
-      belowThresholdNotified = true; // Set the flag to indicate notification sent
-      aboveThresholdNotified = false; // Reset the flag for above threshold notification
+  } else { 
+    if (!belowThresholdNotified) { 
+      belowThresholdNotified = true; 
+      aboveThresholdNotified = false;
 
-      // Disconnect from the server to allow new trigger
       if (client.connected()) {
         client.stop();
         Serial.println("Disconnected from server");
       }
 
-      // Attempt to reconnect to the server
-      if (client.connect(HOST_NAME, 80)) {
+      if (client.connect(HOST_NAME1, 80)) {
         Serial.println("Connected to server");
       } else {
         Serial.println("Connection failed");
       }
 
-      // make a HTTP request for lux < 300 notification:
-      // send HTTP header
+      // Send HTTP request
       client.println("GET " + PATH_NAME1 + queryString + " HTTP/1.1");
       client.println("Host: " + String(HOST_NAME1));
       client.println("Connection: close");
-      client.println(); // end HTTP header
-    
-    delay(1000);
-    
-  }
+      client.println();
+      
+      delay(1000);
+    }
   }
 }
-    
--- secret.h
+++ secret.h
@@ -1,5 +1,3 @@
-#define SECRET_SSID "Patrick"
+#define SECRET_SSID "Vossiepkv"
 #define SECRET_PASS "poopoo123"
 
-#define SECRET_CH_ID 2467060            // replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "NBL8BXU1KPVOI5OW"   // replace XYZ with your channel write API Key
